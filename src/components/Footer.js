1c5f9119808a3a310238f7cabaf4dabc880c1848
theshubhamio <theshubhamiohub@gmail.com>
2024-12-29 22:52:51 +0530

    added components and meta tags

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2023 Little Lemon. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <img src="path/to/logo.png" alt="Little Lemon Logo" />
+    </header>
+  );
+}
+
+export default Header;
